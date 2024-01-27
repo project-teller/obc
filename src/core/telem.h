0afc0e5da2bd3fa257cce9f569fd047db261fd20
Tamas Nepusz <ntamas@gmail.com>
2024-01-27 21:07:42 +0100

    doc: extended docs

--- src/core/telem.h
+++ src/core/telem.h
@@ -15,6 +15,13 @@ typedef enum {
     SCINTILLATOR_MODULE = 3,
 } component_t;
 
+/**
+ * @brief Envelope of a telemetry frame.
+ *
+ * The envelope contains a sequence number, the type of the telemetry frame,
+ * the source component that sends the frame and the target component the
+ * frame is addressed to.
+ */
 typedef struct {
     std::uint8_t seq_no;
     std::uint8_t frame_type;
@@ -35,8 +42,27 @@ namespace frames {
 
 }
 
+/**
+ * Returns the minimum size of the buffer that is needed to hold a message with
+ * the given payload length.
+ *
+ * @param payload_length  the payload length of the message
+ * @return the minimum size of the buffer; basically the payload length plus the
+ *   message framing overhead. Returns zero if the payload is too large.
+ */
 uint8_t getMessageSizeForPayloadLength(std::uint8_t payload_length);
 
+/**
+ * @brief Serializes a telemetry message into a pre-allocated buffer.
+ *
+ * @param buffer  the buffer to write the serialized message into
+ * @param buffer_length  the allocated size of the buffer
+ * @param envelope  the envelope of the telemetry message
+ * @param payload  the payload of the telemetry message
+ * @param payload_length  the length of the payload of the telemetry message
+ * @return the number of bytes actually used from the buffer; zero if there
+ *   was an error while serializing the message.
+ */
 [[nodiscard]] std::uint8_t serialize(
     uint8_t* buffer, std::uint8_t buffer_length, envelope_t envelope,
     const std::uint8_t* payload, std::uint8_t payload_length);
