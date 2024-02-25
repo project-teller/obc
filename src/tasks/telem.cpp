bb334cb7f885703740fb528d23630e0dbc6a9cdf
Tamas Nepusz <ntamas@gmail.com>
2024-02-25 20:36:00 +0100

    feat: heartbeat and time sync messages are now sent

--- src/tasks/telem.cpp
+++ src/tasks/telem.cpp
@@ -13,26 +13,40 @@ const osThreadAttr_t teller::tasks::telemetryTaskAttr = {
     .priority = osPriorityNormal,
 };
 
+static void sendHeartbeat(uint8_t* payload);
+static void sendTimesync(uint8_t* payload);
+
 __NO_RETURN void teller::tasks::telemetryTask(void* arg)
 {
     uint8_t payload[MAX_PAYLOAD_LENGTH];
-    uint8_t to_send;
     uint8_t loop_counter;
-    frames::heartbeat_data_t heartbeat;
-    frames::timesync_data_t timesync;
-
-    memset(&heartbeat, 0, sizeof(heartbeat));
 
     for (loop_counter = 0;; loop_counter++) {
         /* Heartbeat is sent in every iteration, i.e. twice per second */
-        updateHeartbeatData(&heartbeat);
-        to_send = encodeHeartbeatFrame(&heartbeat, payload);
-        send(frames::HEARTBEAT, payload, to_send);
+        sendHeartbeat(payload);
 
         /* Timesync packet is sent once every 8 iterations, i.e. at 4 Hz */
         if (loop_counter % 8 == 0) {
+            sendTimesync(payload);
         }
 
         osDelay(500);
     }
+}
+
+static void sendHeartbeat(uint8_t* payload)
+{
+    frames::heartbeat_data_t heartbeat;
+
+    memset(&heartbeat, 0, sizeof(heartbeat));
+    updateHeartbeatData(&heartbeat);
+    send(frames::HEARTBEAT, payload, encodeHeartbeatFrame(&heartbeat, payload));
+}
+
+static void sendTimesync(uint8_t* payload)
+{
+    frames::timesync_data_t timesync;
+
+    updateTimesyncData(&timesync);
+    send(frames::TIMESYNC, payload, encodeTimesyncFrame(&timesync, payload));
 }
