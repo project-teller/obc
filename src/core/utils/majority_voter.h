2dbf08155c85df2dd955dc2360bcc1836bc472f6
Tamas Nepusz <ntamas@gmail.com>
2024-02-29 10:09:45 +0100

    feat: added majority-in-5 voter module

---
+++ src/core/utils/majority_voter.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <cstdint>
+
+namespace teller::utils {
+
+/**
+ * @brief Majority voting algorithm for binary values.
+ *
+ * This class keeps track of the most recent 5 values of a single bit and
+ * returns the majority of the recorded values.
+ */
+class MajorityVoter {
+public:
+    explicit MajorityVoter(bool initial = false)
+    {
+        reset(initial);
+    }
+
+    /** Feeds a new bit into the majority voter */
+    void feed(bool bit)
+    {
+        history = (history << 1) | (bit ? 1 : 0);
+    }
+
+    /** Returns the current majority value */
+    bool get() const
+    {
+        return MAJORITY_TABLE & (1 << history);
+    }
+
+    /** Resets the voter to a state where it is assumed that the entire
+     * history is filled with the given value. */
+    void reset(bool value = false)
+    {
+        history = value ? MASK : 0;
+    }
+
+private:
+    /** Internal state variable keeping track of the last bits */
+    uint8_t history;
+
+    /** Helper constant to mask the history variable */
+    static const uint8_t MASK = (1 << 5) - 1;
+
+    /** Helper constant where the i-th bit is set if the majority of the last five
+     * recorded bits is true if the last five bits can be represented in binary
+     * as the number i.
+     *
+     * Magic value for 3-majority is: 232
+     * Magic value for 5-majority is: 4276676736
+     *
+     * Python one-liner to validate the values:
+     *
+     * \verbatim
+     * int("".join("1" if x.bit_count() >= 2 else "0" for x in reversed(range(8))), base=2)
+     * int("".join("1" if x.bit_count() >= 3 else "0" for x in reversed(range(32))), base=2)
+     * \endverbatim
+     */
+    static const uint32_t MAJORITY_TABLE = 4276676736;
+};
+
+}
---
+++ test/core/majority_voter_test.cpp
@@ -0,0 +1,58 @@
+#include <cstring>
+#include <gtest/gtest.h>
+
+#include "core/utils/majority_voter.h"
+
+using namespace teller::utils;
+
+TEST(MajorityVoterTest, initialization)
+{
+    MajorityVoter voter;
+
+    EXPECT_FALSE(voter.get());
+
+    voter.feed(true);
+    EXPECT_FALSE(voter.get());
+
+    voter.feed(true);
+    EXPECT_FALSE(voter.get());
+
+    voter.feed(true);
+    EXPECT_TRUE(voter.get());
+
+    voter.reset(true);
+
+    EXPECT_TRUE(voter.get());
+
+    voter.feed(false);
+    EXPECT_TRUE(voter.get());
+
+    voter.feed(false);
+    EXPECT_TRUE(voter.get());
+
+    voter.feed(false);
+    EXPECT_FALSE(voter.get());
+}
+
+TEST(MajorityVoterTest, feedAndGet)
+{
+    MajorityVoter voter;
+    /* fmt: off */
+    /* This is a (2, 5) de Bruijn sequence. It contains all possible 5-bit
+     * substrings while being minimal in length */
+    int sequence[] = {
+        0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0,
+        1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0
+    };
+    int expected[] = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0
+    };
+    constexpr size_t n = sizeof(sequence) / sizeof(sequence[0]);
+    static_assert(sizeof(expected) / sizeof(expected[0]) == n);
+
+    for (size_t i = 0; i < n; i++) {
+        voter.feed(sequence[i]);
+        EXPECT_EQ(expected[i], voter.get() ? 1 : 0) << "Failure at index " << i;
+    }
+}
