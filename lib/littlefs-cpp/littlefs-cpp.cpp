ea6255884013b50814491e492c0da6c9cbef23d8
Tamas Nepusz <ntamas@gmail.com>
2024-03-16 13:36:08 +0100

    feat: added LittleFS C++ wrapper

---
+++ lib/littlefs-cpp/littlefs-cpp.cpp
@@ -0,0 +1,296 @@
+/*
+ * This file is based on the 107-Arduino-littlefs wrapper from the following URL:
+ * https://raw.githubusercontent.com/107-systems/107-Arduino-littlefs/main/src/107-Arduino-littlefs.cpp
+ */
+
+/**
+ * This software is distributed under the terms of the MIT License.
+ * Copyright (c) 2023 LXRobotics.
+ * Author: Alexander Entinger <alexander.entinger@lxrobotics.com>
+ * Contributors: https://github.com/107-systems/107-Arduino-littlefs/graphs/contributors.
+ */
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include "littlefs-cpp.h"
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace littlefs {
+
+/**************************************************************************************
+ * PUBLIC MEMBER FUNCTIONS
+ **************************************************************************************/
+
+#ifndef LFS_READONLY
+std::optional<Error> Filesystem::format()
+{
+    if (auto const err = lfs_format(&_lfs, &_cfg.raw_cfg()); err != LFS_ERR_OK)
+        return static_cast<Error>(err);
+
+    return std::nullopt;
+}
+#endif
+
+std::optional<Error> Filesystem::mount()
+{
+    if (auto const err = lfs_mount(&_lfs, &_cfg.raw_cfg()); err != LFS_ERR_OK)
+        return static_cast<Error>(err);
+
+    return std::nullopt;
+}
+
+std::optional<Error> Filesystem::unmount()
+{
+    if (auto const err = lfs_unmount(&_lfs); err != LFS_ERR_OK)
+        return static_cast<Error>(err);
+
+    return std::nullopt;
+}
+
+#ifndef LFS_READONLY
+std::optional<Error> Filesystem::remove(std::string const& path)
+{
+    if (auto const err = lfs_remove(&_lfs, path.c_str()); err != LFS_ERR_OK)
+        return static_cast<Error>(err);
+
+    return std::nullopt;
+}
+
+std::optional<Error> Filesystem::rename(std::string const& old_path, std::string const& new_path)
+{
+    if (auto const err = lfs_rename(&_lfs, old_path.c_str(), new_path.c_str()); err != LFS_ERR_OK)
+        return static_cast<Error>(err);
+
+    return std::nullopt;
+}
+#endif
+
+std::variant<Error, FileHandle> Filesystem::open(std::string const& path, OpenFlag const flags)
+{
+    auto file_hdl = std::make_shared<lfs_file_t>();
+
+    int const rc = lfs_file_open(&_lfs, file_hdl.get(), path.c_str(), static_cast<int>(flags));
+
+    if (rc < LFS_ERR_OK)
+        return static_cast<Error>(rc);
+
+    _file_desc_map[_file_dsc_cnt++] = file_hdl;
+    return (_file_dsc_cnt - 1);
+}
+
+std::variant<Error, size_t> Filesystem::read(FileHandle const fd, void* read_buf, size_t const bytes_to_read)
+{
+    auto iter = _file_desc_map.find(fd);
+    if (iter == _file_desc_map.end())
+        return Error::NO_FD_ENTRY;
+
+    int const rc = lfs_file_read(&_lfs, iter->second.get(), read_buf, bytes_to_read);
+
+    if (rc < LFS_ERR_OK)
+        return static_cast<Error>(rc);
+
+    return static_cast<size_t>(rc);
+}
+
+#ifndef LFS_READONLY
+std::variant<Error, size_t> Filesystem::write(FileHandle const fd, void const* write_buf, size_t const bytes_to_write)
+{
+    auto iter = _file_desc_map.find(fd);
+    if (iter == _file_desc_map.end())
+        return Error::NO_FD_ENTRY;
+
+    int const rc = lfs_file_write(&_lfs, iter->second.get(), write_buf, bytes_to_write);
+
+    if (rc < LFS_ERR_OK)
+        return static_cast<Error>(rc);
+
+    return static_cast<size_t>(rc);
+}
+#endif
+
+#ifndef LFS_READONLY
+std::optional<Error> Filesystem::truncate(FileHandle const fd, int const size)
+{
+    auto iter = _file_desc_map.find(fd);
+    if (iter == _file_desc_map.end())
+        return Error::NO_FD_ENTRY;
+
+    if (auto const err = lfs_file_truncate(&_lfs, iter->second.get(), size); err != LFS_ERR_OK)
+        return static_cast<Error>(err);
+
+    return std::nullopt;
+}
+#endif
+
+std::variant<Error, size_t> Filesystem::tell(FileHandle const fd)
+{
+    auto iter = _file_desc_map.find(fd);
+    if (iter == _file_desc_map.end())
+        return Error::NO_FD_ENTRY;
+
+    int const rc = lfs_file_tell(&_lfs, iter->second.get());
+
+    if (rc < LFS_ERR_OK)
+        return static_cast<Error>(rc);
+
+    return static_cast<size_t>(rc);
+}
+
+std::variant<Error, size_t> Filesystem::size(FileHandle const fd)
+{
+    auto iter = _file_desc_map.find(fd);
+    if (iter == _file_desc_map.end())
+        return Error::NO_FD_ENTRY;
+
+    int const rc = lfs_file_size(&_lfs, iter->second.get());
+
+    if (rc < LFS_ERR_OK)
+        return static_cast<Error>(rc);
+
+    return static_cast<size_t>(rc);
+}
+
+std::variant<Error, size_t> Filesystem::seek(FileHandle const fd, int const offset, WhenceFlag const whence)
+{
+    auto iter = _file_desc_map.find(fd);
+    if (iter == _file_desc_map.end())
+        return Error::NO_FD_ENTRY;
+
+    int const rc = lfs_file_seek(&_lfs, iter->second.get(), offset, static_cast<int>(whence));
+
+    if (rc < LFS_ERR_OK)
+        return static_cast<Error>(rc);
+
+    return static_cast<size_t>(rc);
+}
+
+std::optional<Error> Filesystem::rewind(FileHandle const fd)
+{
+    auto iter = _file_desc_map.find(fd);
+    if (iter == _file_desc_map.end())
+        return Error::NO_FD_ENTRY;
+
+    if (auto const err = lfs_file_rewind(&_lfs, iter->second.get()); err != LFS_ERR_OK)
+        return static_cast<Error>(err);
+
+    return std::nullopt;
+}
+
+std::optional<Error> Filesystem::sync(FileHandle const fd)
+{
+    auto iter = _file_desc_map.find(fd);
+    if (iter == _file_desc_map.end())
+        return Error::NO_FD_ENTRY;
+
+    if (auto const err = lfs_file_sync(&_lfs, iter->second.get()); err != LFS_ERR_OK)
+        return static_cast<Error>(err);
+
+    return std::nullopt;
+}
+
+std::optional<Error> Filesystem::close(FileHandle const fd)
+{
+    auto iter = _file_desc_map.find(fd);
+    if (iter == _file_desc_map.end())
+        return Error::NO_FD_ENTRY;
+
+    _file_desc_map.erase(fd);
+
+    if (auto const err = lfs_file_close(&_lfs, iter->second.get()); err != LFS_ERR_OK)
+        return static_cast<Error>(err);
+
+    return std::nullopt;
+}
+
+#ifndef LFS_READONLY
+std::optional<Error> Filesystem::mkdir(std::string const& path)
+{
+    if (auto const err = lfs_mkdir(&_lfs, path.c_str()); err != LFS_ERR_OK)
+        return static_cast<Error>(err);
+
+    return std::nullopt;
+}
+#endif
+
+std::variant<Error, DirHandle> Filesystem::dir_open(std::string const& path)
+{
+    auto dir_hdl = std::make_shared<lfs_dir_t>();
+
+    int const rc = lfs_dir_open(&_lfs, dir_hdl.get(), path.c_str());
+
+    if (rc < LFS_ERR_OK)
+        return static_cast<Error>(rc);
+
+    _dir_desc_map[_dir_dsc_cnt++] = dir_hdl;
+    return (_dir_dsc_cnt - 1);
+}
+
+std::optional<Error> Filesystem::dir_close(DirHandle const dd)
+{
+    auto iter = _dir_desc_map.find(dd);
+    if (iter == _dir_desc_map.end())
+        return Error::NO_DD_ENTRY;
+
+    _dir_desc_map.erase(dd);
+
+    if (auto const err = lfs_dir_close(&_lfs, iter->second.get()); err != LFS_ERR_OK)
+        return static_cast<Error>(err);
+
+    return std::nullopt;
+}
+
+std::variant<Error, size_t> Filesystem::dir_read(DirHandle const dd, std::string& name, Type& type)
+{
+    auto iter = _dir_desc_map.find(dd);
+    if (iter == _dir_desc_map.end())
+        return Error::NO_DD_ENTRY;
+
+    lfs_info info;
+    int const rc = lfs_dir_read(&_lfs, iter->second.get(), &info);
+
+    // Note: lfs_dir_read returns false (0) when no more entries, true (1) on success,
+    // and possibly some lfs_error.
+    if (rc == 0)
+        return Error::NOENT;
+
+    if (rc < LFS_ERR_OK)
+        return static_cast<Error>(rc);
+
+    name = std::string(info.name);
+    type = static_cast<Type>(info.type);
+
+    return type == Type::REG ? static_cast<size_t>(info.size) : 0;
+}
+
+std::optional<Error> Filesystem::dir_rewind(FileHandle const dd)
+{
+    auto iter = _dir_desc_map.find(dd);
+    if (iter == _dir_desc_map.end())
+        return Error::NO_DD_ENTRY;
+
+    if (auto const err = lfs_dir_rewind(&_lfs, iter->second.get()); err != LFS_ERR_OK)
+        return static_cast<Error>(err);
+
+    return std::nullopt;
+}
+
+std::variant<Error, size_t> Filesystem::fs_size()
+{
+    int const rc = lfs_fs_size(&_lfs);
+
+    if (rc < LFS_ERR_OK)
+        return static_cast<Error>(rc);
+
+    return static_cast<size_t>(rc);
+}
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* littlefs */
---
+++ lib/littlefs-cpp/littlefs-cpp.h
@@ -0,0 +1,198 @@
+/*
+ * This file is based on the 107-Arduino-littlefs wrapper from the following URL:
+ * https://raw.githubusercontent.com/107-systems/107-Arduino-littlefs/main/src/107-Arduino-littlefs.h
+ */
+
+/**
+ * This software is distributed under the terms of the MIT License.
+ * Copyright (c) 2023 LXRobotics.
+ * Author: Alexander Entinger <alexander.entinger@lxrobotics.com>
+ * Contributors: https://github.com/107-systems/107-Arduino-littlefs/graphs/contributors.
+ */
+
+#ifndef _107_ARDUINO_LITTLEFS_H_
+#define _107_ARDUINO_LITTLEFS_H_
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include "lfs.h"
+
+#include <map>
+#include <memory>
+#include <optional>
+#include <string>
+#include <variant>
+
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+namespace littlefs {
+
+/**************************************************************************************
+ * TYPEDEF
+ **************************************************************************************/
+
+enum class OpenFlag : int {
+    RDONLY = LFS_O_RDONLY,
+#ifndef LFS_READONLY
+    WRONLY = LFS_O_WRONLY,
+    RDWR = LFS_O_RDWR,
+    CREAT = LFS_O_CREAT,
+    EXCL = LFS_O_EXCL,
+    TRUNC = LFS_O_TRUNC,
+    APPEND = LFS_O_APPEND,
+#endif
+};
+
+enum class WhenceFlag : int {
+    SET = LFS_SEEK_SET,
+    CUR = LFS_SEEK_CUR,
+    END = LFS_SEEK_END,
+};
+
+enum class Error : int {
+    IO = LFS_ERR_IO,
+    CORRUPT = LFS_ERR_CORRUPT,
+    NOENT = LFS_ERR_NOENT,
+    EXIST = LFS_ERR_EXIST,
+    NOTDIR = LFS_ERR_NOTDIR,
+    ISDIR = LFS_ERR_ISDIR,
+    NOTEMPTY = LFS_ERR_NOTEMPTY,
+    BADF = LFS_ERR_BADF,
+    FBIG = LFS_ERR_FBIG,
+    INVAL = LFS_ERR_INVAL,
+    NOSPC = LFS_ERR_NOSPC,
+    NOMEM = LFS_ERR_NOMEM,
+    NOATTR = LFS_ERR_NOATTR,
+    NAMETOOLONG = LFS_ERR_NAMETOOLONG,
+    NO_FD_ENTRY = -50, // No entry found for given file descriptor
+    NO_DD_ENTRY = -51, // No entry found for given directory descriptor
+};
+
+enum class Type : int {
+    REG = LFS_TYPE_REG,
+    DIR = LFS_TYPE_DIR,
+};
+
+typedef size_t FileHandle;
+typedef size_t DirHandle;
+
+/**************************************************************************************
+ * CLASS DECLARATION
+ **************************************************************************************/
+
+class FilesystemConfig {
+private:
+    lfs_config _cfg;
+
+public:
+    typedef int (*ReadFuncPtr)(const struct lfs_config*, lfs_block_t, lfs_off_t, void*, lfs_size_t);
+    typedef int (*ProgFuncPtr)(const struct lfs_config*, lfs_block_t, lfs_off_t, const void*, lfs_size_t);
+    typedef int (*EraseFuncPtr)(const struct lfs_config*, lfs_block_t);
+    typedef int (*SyncFuncPtr)(const struct lfs_config*);
+
+    FilesystemConfig(ReadFuncPtr read_func,
+        ProgFuncPtr prog_func,
+        EraseFuncPtr erase_func,
+        SyncFuncPtr sync_func,
+        lfs_size_t const read_size,
+        lfs_size_t const prog_size,
+        lfs_size_t const block_size,
+        lfs_size_t const block_count,
+        int32_t const block_cycles,
+        lfs_size_t const cache_size,
+        lfs_size_t const lookahead_size)
+    {
+        memset(&_cfg, 0, sizeof(_cfg));
+
+        _cfg.read = read_func;
+        _cfg.prog = prog_func;
+        _cfg.erase = erase_func;
+        _cfg.sync = sync_func;
+
+        _cfg.read_size = read_size;
+        _cfg.prog_size = prog_size;
+        _cfg.block_size = block_size;
+        _cfg.block_count = block_count;
+        _cfg.block_cycles = block_cycles;
+        _cfg.cache_size = cache_size;
+        _cfg.lookahead_size = lookahead_size;
+    }
+
+    [[nodiscard]] lfs_config& raw_cfg() { return _cfg; }
+};
+
+class Filesystem {
+private:
+    FilesystemConfig& _cfg;
+    lfs_t _lfs;
+    size_t _file_dsc_cnt;
+    std::map<size_t, std::shared_ptr<lfs_file_t>> _file_desc_map;
+    size_t _dir_dsc_cnt;
+    std::map<size_t, std::shared_ptr<lfs_dir_t>> _dir_desc_map;
+
+public:
+    Filesystem(FilesystemConfig& cfg)
+        : _cfg { cfg }
+        , _file_dsc_cnt { 0 }
+        , _dir_dsc_cnt { 0 }
+    {
+        memset(&_lfs, 0, sizeof(_lfs));
+    }
+
+#ifndef LFS_READONLY
+    [[nodiscard]] std::optional<Error> format();
+#endif
+    [[nodiscard]] std::optional<Error> mount();
+    [[nodiscard]] std::optional<Error> unmount();
+
+#ifndef LFS_READONLY
+    [[nodiscard]] std::optional<Error> remove(std::string const& path);
+    [[nodiscard]] std::optional<Error> rename(std::string const& old_path, std::string const& new_path);
+#endif
+
+    [[nodiscard]] std::variant<Error, FileHandle> open(std::string const& path, OpenFlag const flags);
+    [[nodiscard]] std::optional<Error> sync(FileHandle const fd);
+    [[nodiscard]] std::optional<Error> close(FileHandle const fd);
+
+    [[nodiscard]] std::variant<Error, size_t> read(FileHandle const fd, void* read_buf, size_t const bytes_to_read);
+#ifndef LFS_READONLY
+    [[nodiscard]] std::variant<Error, size_t> write(FileHandle const fd, void const* write_buf, size_t const bytes_to_write);
+    [[nodiscard]] std::optional<Error> truncate(FileHandle const fd, int const size);
+#endif
+
+    [[nodiscard]] std::variant<Error, size_t> tell(FileHandle const fd);
+    [[nodiscard]] std::variant<Error, size_t> size(FileHandle const fd);
+    [[nodiscard]] std::variant<Error, size_t> seek(FileHandle const fd, int const offset, WhenceFlag const whence);
+    [[nodiscard]] std::optional<Error> rewind(FileHandle const fd);
+
+#ifndef LFS_READONLY
+    [[nodiscard]] std::optional<Error> mkdir(std::string const& path);
+#endif
+
+    [[nodiscard]] std::variant<Error, DirHandle> dir_open(std::string const& path);
+    [[nodiscard]] std::optional<Error> dir_close(DirHandle const dd);
+    [[nodiscard]] std::variant<Error, size_t> dir_read(DirHandle const dd, std::string& name, Type& type);
+    [[nodiscard]] std::optional<Error> dir_rewind(DirHandle const dd);
+
+    [[nodiscard]] std::variant<Error, size_t> fs_size();
+};
+
+/**************************************************************************************
+ * FREE FUNCTIONS
+ **************************************************************************************/
+
+constexpr OpenFlag operator|(OpenFlag const& lhs, OpenFlag const& rhs)
+{
+    return static_cast<OpenFlag>(static_cast<int>(lhs) | static_cast<int>(rhs));
+}
+/**************************************************************************************
+ * NAMESPACE
+ **************************************************************************************/
+
+} /* littlefs */
+
+#endif /* _107_ARDUINO_LITTLEFS_H_ */
