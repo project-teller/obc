eb821a8feb710197049e52297198f1c6c589408b
Tamas Nepusz <ntamas@gmail.com>
2024-09-21 14:30:05 +0200

    feat: add UART RX pullup to avoid a floating pin when it is not connected

--- src/hal/stm32/uart.cpp
+++ src/hal/stm32/uart.cpp
@@ -544,7 +544,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
             enableGPIOClocksForPort(cfg->gpio.by_index[i].port);
             GPIO_InitStruct.Pin = cfg->gpio.by_index[i].pins;
             GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-            GPIO_InitStruct.Pull = GPIO_NOPULL;
+            GPIO_InitStruct.Pull = i == 1 ? GPIO_PULLUP : GPIO_NOPULL;
             GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
             GPIO_InitStruct.Alternate = gpioFunc;
             HAL_GPIO_Init(cfg->gpio.by_index[i].port, &GPIO_InitStruct);
