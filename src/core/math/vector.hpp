7d9e356d5b3f5f751f9dc4ec833517467dcf767e
Tamas Nepusz <ntamas@gmail.com>
2024-10-04 02:43:11 +0200

    fix: fix unit test compilation error

--- src/core/math/vector.hpp
+++ src/core/math/vector.hpp
@@ -96,10 +96,8 @@ public:
             return x;
         case 1:
             return y;
-        case 2:
-            return z;
         default:
-            return T();
+            return z;
         }
     }
 
@@ -110,10 +108,8 @@ public:
             return x;
         case 1:
             return y;
-        case 2:
-            return z;
         default:
-            return T();
+            return z;
         }
     }
 
