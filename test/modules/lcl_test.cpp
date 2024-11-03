9a3b937f4204c5fdc0e95d351366b179aaaa6748
Tamas Nepusz <ntamas@gmail.com>
2024-11-03 16:08:07 +0100

    fix: fix memory leak in LCL test

--- test/modules/lcl_test.cpp
+++ test/modules/lcl_test.cpp
@@ -3,40 +3,47 @@
 
 #include "hal/gpio.h"
 #include "modules/lcl.h"
+#include "modules/log.h"
+#include "modules/telem.h"
 
-using namespace teller::lcl;
+using namespace teller;
+using teller::lcl::lcl_t;
 
 class LCLTest : public testing::Test {
 protected:
     void SetUp() override
     {
-        ASSERT_TRUE(teller::hal::gpio::init());
-        ASSERT_TRUE(init());
-        setResetPulseDurationMsec(1);
+        ASSERT_TRUE(hal::gpio::init());
+        ASSERT_TRUE(telem::init());
+        ASSERT_TRUE(log::init());
+        ASSERT_TRUE(lcl::init());
+        lcl::setResetPulseDurationMsec(1);
     }
 
     void TearDown() override
     {
-        destroy();
-        teller::hal::gpio::destroy();
+        log::destroy();
+        telem::destroy();
+        lcl::destroy();
+        hal::gpio::destroy();
     }
 };
 
 TEST_F(LCLTest, areTriggeredAfterBoot)
 {
-    for (int i = 0; i < NUM_LCLS; i++) {
-        ASSERT_TRUE(triggered(static_cast<lcl_t>(i)));
+    for (int i = 0; i < lcl::NUM_LCLS; i++) {
+        ASSERT_TRUE(lcl::triggered(static_cast<lcl_t>(i)));
     }
 
     /* Test invalid value */
-    ASSERT_FALSE(triggered(NUM_LCLS));
+    ASSERT_FALSE(lcl::triggered(lcl::NUM_LCLS));
 }
 
 TEST_F(LCLTest, resetLCL)
 {
-    for (int i = 0; i < NUM_LCLS; i++) {
-        ASSERT_TRUE(triggered(static_cast<lcl_t>(i)));
-        reset(static_cast<lcl_t>(i));
-        ASSERT_FALSE(triggered(static_cast<lcl_t>(i)));
+    for (int i = 0; i < lcl::NUM_LCLS; i++) {
+        ASSERT_TRUE(lcl::triggered(static_cast<lcl_t>(i)));
+        lcl::reset(static_cast<lcl_t>(i));
+        ASSERT_FALSE(lcl::triggered(static_cast<lcl_t>(i)));
     }
 }
