29a76911228fe751140ecc77df4991bb1359aeb3
Tamas Nepusz <ntamas@gmail.com>
2024-11-06 12:49:49 +0100

    fix: updated ADC current scaling factors

--- src/modules/adc.cpp
+++ src/modules/adc.cpp
@@ -35,9 +35,9 @@ static const float scaling[NUM_CHANNELS] = {
     0.488f, /* CUR_SUC_LCL1 */
     0.488f, /* CUR_SUC_LCL2 */
     0.488f, /* CUR_SUC_LCL3 */
-    1.0f, /* CUR_CAM: TODO */
+    9.766f, /* CUR_CAM */
     4.883f, /* CUR_SCM */
-    1.0f, /* CUR_GMM: TODO */
+    9.766f, /* CUR_GMM */
     31.0f, /* 60V_MEAS1 */
     31.0f, /* 60V_MEAS2 */
     31.0f, /* 60V_MEAS3 */
